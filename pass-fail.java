9d5b83e9016cc6cce3004e80d0cf090f23a264a8
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 15:16:58 +0530

    Collect Percentage From user check result
    Check Result Using if - else , Conditional Statement

---
+++ pass-fail.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Percentage:-");
+    int x = sc.nextInt();
+  if(x > 33){
+      System.out.println("You Are Pass !");
+   }
+   else{
+     System.out.println("You Are Fail !");
+   }
+  }
+}
