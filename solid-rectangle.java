a38a9efbf4b9ecfc6ad672596cc5ee81c50f84f1
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 17:16:42 +0530

    Print The Pattern Solid Rectangle
    *****
    *****
    *****
    *****
    Like This .

---
+++ solid-rectangle.java
@@ -0,0 +1,13 @@
+class main{
+    public static void main(String[] args){
+        int n = 4 ;
+        int m = 5 ;
+        
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1;i<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
