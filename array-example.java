72811ff8af9533e61b4f537848346fd016eb8ed0
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 18:09:36 +0530

    array example
    Define  & Declare Array .
    Its a Example of Pre-define Array .

---
+++ array-example.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args){
+        int[] marks = new int[3];
+        marks[0] = 96 ; // first subject marks
+        marks[1] = 91 ; // second subject marks
+        marks[2] = 93 ; // third subject marks
+        System.out.print("Maths Marks = " + marks[0] + "\n English Marks = " + marks[1] + "\n Science Marks = " + marks[2] );
+    }
+}
