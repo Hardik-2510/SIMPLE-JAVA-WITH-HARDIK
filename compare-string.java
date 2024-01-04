f97efd1abf546b89d426b6b768c6e68d7b70812f
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 18:33:50 +0530

    compare string
    This Function use to compare two string each other .

---
+++ compare-string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class main{
+    public static void main(String[] args){
+        String name1 = "Hardik";
+        String name2 = "Hardik";
+        
+        if(name1.compareTo(name2) == 0){
+            System.out.println("Strings Are Equal !!");
+        }
+        else{
+            System.out.println("Strings Are Not Equal !!");
+        }
+    }
+}
