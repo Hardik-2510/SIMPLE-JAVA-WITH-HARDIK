fae51b0fdd01f49509bee0993885553633495bc4
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 17:13:56 +0530

    Do While Loop
    This is a Do While Loop . In This Loop For  A Once Code Execute After That Check The Condition .

---
+++ do-while.java
@@ -0,0 +1,11 @@
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // The body of the loop is executed at least once
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
