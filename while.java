c27d6dd6be6c40c1335564116cc31280aa41f204
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 17:02:55 +0530

    While Loop
    This is a Looping Statement  , First Check the Condition if True , Execute Statement , if condition false exit to the loop.

---
+++ while.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class main{
+    public static void main(String[] args){
+        int n = 0;
+        while(n < 11){
+            System.out.println(n);
+            n++;
+        }
+    }
+}
