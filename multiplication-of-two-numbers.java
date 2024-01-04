43fd6b16c22ef2e5a308c78d6315129aa5336f8d
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 13:35:13 +0530

    Update multiplication-of-two-numbers.java

--- multiplication-of-two-numbers.java
+++ multiplication-of-two-numbers.java
@@ -1,6 +1,6 @@
 import java.util.* ; 
 
-public class sum{
+public class multiplication{
     public static void main(String[] args){
   Scanner sc = new Scanner(System.in);
   System.out.print("Enter Number 1 :-  ");
