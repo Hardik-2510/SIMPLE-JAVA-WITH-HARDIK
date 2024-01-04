b931ec635f625cc1d46323ae4818f3c70bbc40ff
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 18:30:37 +0530

    length string
    This function use to find the length of string

---
+++ length-string.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args){
+        // concatination 
+        String firstName = "Hardik";
+        String lastName = "Patel";
+        String fullName = firstName + " " +  lastName ;
+        System.out.println(fullName.length();
+    }
+}
