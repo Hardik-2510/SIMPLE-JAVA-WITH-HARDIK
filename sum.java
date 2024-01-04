0de7a92c9a3a38edf8a56beb88ba4be3013005c6
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 13:09:48 +0530

    sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.* ; 
+
+public class sum{
+    public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter Number 1 :-  ");
+  int x = sc.nextInt();
+  System.out.print("Enter Number 2 :-  ");
+  int y = sc.nextInt();
+  int z = x + y ;
+  System.out.print(" Sum of " + x + " + " + y + " = " + z);
+    }
+}
