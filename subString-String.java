b290293be00837d255fc5500175a67d166b89ef5
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-04 18:35:05 +0530

    subString String
    To Take Some Part Of String

---
+++ subString-String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class main{
+    public static void main(String[] args){
+        String sentence = "My name is Hardik";
+        String name = sentence.substring(11,sentence.length());
+        System.out.println(name);
+    }
+}
